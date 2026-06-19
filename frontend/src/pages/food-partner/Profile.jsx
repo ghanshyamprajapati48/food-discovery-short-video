f7e9711524f3e20249f941b86e6cb16475a00b74
ghanshyamprajapati48 <ghanshyamprajapati4848@gmail.com>
2026-06-19 16:09:08 +0530

    Update API endpoint for food partner profile

--- frontend/src/pages/food-partner/Profile.jsx
+++ frontend/src/pages/food-partner/Profile.jsx
@@ -9,7 +9,7 @@ const Profile = () => {
     const [ videos, setVideos ] = useState([])
 
     useEffect(() => {
-        axios.get(`http://localhost:3000/api/food-partner/${id}`, { withCredentials: true })
+        axios.get(`https://food-discovery-short-video-back.onrender.com/api/food-partner/${id}`, { withCredentials: true })
             .then(response => {
                 setProfile(response.data.foodPartner)
                 setVideos(response.data.foodPartner.foodItems)
